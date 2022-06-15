1f5947cbfc3fff4e31f777dd813f86e436a0263f
Anandu S Nande <anandhu66330@gmail.com>
2022-06-15 23:48:11 +0530

    creted Team model and super user and team shown in first_name based

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from pages.models import Team
+from .models import Team
 
 # Register your models here.
+admin.site.register(Team)
+
--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,27 @@
+from distutils.command.upload import upload
+from pickle import TRUE
+from tabnanny import verbose
 from django.db import models
 
 # Create your models here.
+
+class Team(models.Model):
+
+    first_name = models.CharField(max_length=225)
+    last_name = models.CharField(max_length=255)
+    designation = models.CharField(max_length=255)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    facebook_link = models.URLField(max_length=100)
+    twitter_link = models.URLField(max_length=100)
+    google_plus_link = models.URLField(max_length=100)
+    created_data = models.DateTimeField(auto_now_add=TRUE)
+
+    def __str__(self):
+        return self.first_name  #THIS IS FOR ADMIN PANNEL'S TEAMS SHOWN BASED ON FIRST_NAME
+        
+
+    class Meta:
+        verbose_name = 'Teams'
+        verbose_name_plural = 'Team'  #THIS IS FOR ADMIN PANNEL NAME CHANGE
+    
+   
